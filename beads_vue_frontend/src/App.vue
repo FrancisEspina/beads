7c464f4e8cb15a93ffc76a996c63bbd696673883
Francis Lorenzo <faespina@up.edu.ph>
2024-10-10 04:12:25 +0800

    beadss

---
+++ beads_vue_frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout class="">
+    <v-navigation-drawer rail>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar elevation="0" title="Application bar"></v-app-bar>
+
+    <v-main
+      class="d-flex align-center justify-center"
+      style="min-height: 300px"
+    >
+      <router-view></router-view>
+    </v-main>
+  </v-layout>
+</template>
+
+<script setup>
+//
+</script>
